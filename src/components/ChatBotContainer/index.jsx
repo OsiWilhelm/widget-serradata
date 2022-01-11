4834e0382d92da938a23d1c3b8d8fdc14de2b2b1
Giulia <giu.olivers@gmail.com>
2022-01-11 12:46:37 -0300

    Implementa interação textual

--- src/components/ChatBotContainer/index.jsx
+++ src/components/ChatBotContainer/index.jsx
@@ -18,36 +18,46 @@ export default function ChatBotContainer() {
 
   const criarFluxo = (intencaoId = 1) => {
     let intencao = [];
-    let opcoes = [];
+    let opcoes = { Null: { options: [] } };
 
     if (intencoes.length > 0) {
-      intencao = intencoes.filter((i) => i.id == intencaoId);
+      intencao = intencoes.filter((i) => i.id === intencaoId);
 
       steps.push({
         id: intencaoId,
         message: intencao[0].titulo,
+        trigger: `${intencaoId}-opcoesMsg`,
+      });
+
+      steps.push({
+        id: `${intencaoId}-opcoesMsg`,
+        message: () => {
+          let opcoesMsg = "";
+
+          intencao[0].opcoes.map(
+            (opcao, i) => (opcoesMsg += `\n${i + 1}) ${opcao.descricao}\n`)
+          );
+          return `${opcoesMsg}`;
+        },
         trigger: `${intencaoId}-opcoes`,
       });
 
       if (intencao[0].opcoes) {
         intencao[0].opcoes.forEach((opcao, i) => {
           if (opcao.proximaIntencao != null) {
-            opcoes.push({
-              value: i + 1,
-              label: opcao.descricao,
-              trigger: opcao.proximaIntencao.id,
-            });
+            opcoes.hasOwnProperty(opcao.proximaIntencao.id)
+              ? opcoes[opcao.proximaIntencao.id].options.push(opcao.descricao)
+              : (opcoes[opcao.proximaIntencao.id] = {
+                  options: [opcao.descricao],
+                });
 
-            if (steps.filter((i) => i.id == opcao.proximaIntencao.id) == 0) {
+            if (
+              steps.filter((i) => i.id === opcao.proximaIntencao.id).length ===
+              0
+            ) {
               criarFluxo(opcao.proximaIntencao.id);
             }
           } else {
-            opcoes.push({
-              value: i + 1,
-              label: opcao.descricao,
-              trigger: `solucao${intencaoId}-${i + 1}`,
-            });
-
             steps.push({
               id: `solucao${intencaoId}-${i + 1}`,
               component: (
@@ -67,13 +77,41 @@ export default function ChatBotContainer() {
 
       steps.push({
         id: `${intencaoId}-opcoes`,
-        options: opcoes,
+        user: true,
+        validator: (value) => {
+          const options = [...intencao[0].opcoes];
+
+          return (!isNaN(value) &&
+            parseInt(value) > 0 &&
+            parseInt(value) <= options.length) ||
+            (isNaN(value) &&
+              options.some(
+                (opcao) => opcao.descricao.toLowerCase() === value.toLowerCase()
+              ))
+            ? true
+            : "Resposta inválida!";
+        },
+        trigger: ({ value }) => {
+          const options = [...intencao[0].opcoes];
+          let op;
+
+          isNaN(value)
+            ? (op = options.find(
+                (opcao) => opcao.descricao.toLowerCase() === value.toLowerCase()
+              ))
+            : (op = options[value - 1]);
+
+          return op.proximaIntencao
+            ? op.proximaIntencao.id
+            : `solucao${intencaoId}-${options.indexOf(op) + 1}`;
+        },
       });
     }
   };
 
   const criarChatBot = () => {
     criarFluxo();
+
     return (
       <ChatBoot
         headerTitle="SerraData"
