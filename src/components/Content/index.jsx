cd34335917dc6efce6342d5c1d2d971a6a317cee
Giulia <giu.olivers@gmail.com>
2022-01-17 01:00:32 -0300

    Corrige renderização de imagens e estilos na solução final

--- src/components/Content/index.jsx
+++ src/components/Content/index.jsx
@@ -1,22 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { convertFromRaw } from 'draft-js';
-import { convertToHTML } from 'draft-convert';
+import React, { useEffect, useState } from "react";
+import { convertFromRaw } from "draft-js";
+import { convertToHTML } from "draft-convert";
 
-import HtmlReactParser from 'html-react-parser';
+import HtmlReactParser from "html-react-parser";
 
-import { Container } from './styles';
+import { Container } from "./styles";
 
 export const Content = ({ element }) => {
-    const [componente, setComponente] = useState();
+  const [componente, setComponente] = useState();
 
+  useEffect(() => {
+    setComponente(element);
+  }, []);
 
-    useEffect(() => {
-        const elementHTML = HtmlReactParser(convertToHTML(convertFromRaw(JSON.parse(element))));
-
-        setComponente(elementHTML);
-    }, []);
-
-    return <Container>{componente}</Container>
-} 
+  return <Container>{HtmlReactParser(componente)}</Container>;
+};
 
 export default Content;
